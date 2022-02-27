07b28edb0f19badc6f7de7d123e3958988395737
Daniela Paiva <danielapv200@gmail.com>
2022-02-27 01:54:17 -0500

    bk 27/02

--- components/calculadoraArbitraje.js
+++ components/calculadoraArbitraje.js
@@ -167,7 +167,7 @@ export default function CalculadoraArbitraje() {
                                     as="h3"
                                     className="text-2xl leading-6 font-bold text-gray-800 text-center"
                                 >
-                                    Calculadora de Gastos Arbirtales
+                                    Calculadora de Gastos Arbitrales
                                     <button onClick={closeModal} className="absolute top-2 right-2 hover:bg-gray-200 p-1 rounded-full transition-all duration-300">
                                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                                             <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />

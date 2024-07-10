c5289fbf9c0f29f01c36502487d4ef74d9a7a5e2
Daniela Paiva <danielap@neuronaagency.com>
2024-07-10 18:27:44 -0500

    fix secretarias arbitrales

--- pages/solucion-de-disputas/centro-de-arbitraje.js
+++ pages/solucion-de-disputas/centro-de-arbitraje.js
@@ -192,7 +192,7 @@ var options = [
     icon: (
       <svg 
       className="my-auto h-7 w-7 text-blue-600"
-      viewBox="0 0 640 512" height="48" width="48" focusable="false" role="img" fill="currentColor" xmlns="http://www.w3.org/2000/svg" class="css-1eamic5 ex0cdmw0"><path fill="currentColor" d="M256 336h-.02c0-16.18 1.34-8.73-85.05-181.51-17.65-35.29-68.19-35.36-85.87 0C-2.06 328.75.02 320.33.02 336H0c0 44.18 57.31 80 128 80s128-35.82 128-80zM128 176l72 144H56l72-144zm511.98 160c0-16.18 1.34-8.73-85.05-181.51-17.65-35.29-68.19-35.36-85.87 0-87.12 174.26-85.04 165.84-85.04 181.51H384c0 44.18 57.31 80 128 80s128-35.82 128-80h-.02zM440 320l72-144 72 144H440zm88 128H352V153.25c23.51-10.29 41.16-31.48 46.39-57.25H528c8.84 0 16-7.16 16-16V48c0-8.84-7.16-16-16-16H383.64C369.04 12.68 346.09 0 320 0s-49.04 12.68-63.64 32H112c-8.84 0-16 7.16-16 16v32c0 8.84 7.16 16 16 16h129.61c5.23 25.76 22.87 46.96 46.39 57.25V448H112c-8.84 0-16 7.16-16 16v32c0 8.84 7.16 16 16 16h416c8.84 0 16-7.16 16-16v-32c0-8.84-7.16-16-16-16z"></path></svg>
+      viewBox="0 0 640 512" height="48" width="48" focusable="false" role="img" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill="currentColor" d="M256 336h-.02c0-16.18 1.34-8.73-85.05-181.51-17.65-35.29-68.19-35.36-85.87 0C-2.06 328.75.02 320.33.02 336H0c0 44.18 57.31 80 128 80s128-35.82 128-80zM128 176l72 144H56l72-144zm511.98 160c0-16.18 1.34-8.73-85.05-181.51-17.65-35.29-68.19-35.36-85.87 0-87.12 174.26-85.04 165.84-85.04 181.51H384c0 44.18 57.31 80 128 80s128-35.82 128-80h-.02zM440 320l72-144 72 144H440zm88 128H352V153.25c23.51-10.29 41.16-31.48 46.39-57.25H528c8.84 0 16-7.16 16-16V48c0-8.84-7.16-16-16-16H383.64C369.04 12.68 346.09 0 320 0s-49.04 12.68-63.64 32H112c-8.84 0-16 7.16-16 16v32c0 8.84 7.16 16 16 16h129.61c5.23 25.76 22.87 46.96 46.39 57.25V448H112c-8.84 0-16 7.16-16 16v32c0 8.84 7.16 16 16 16h416c8.84 0 16-7.16 16-16v-32c0-8.84-7.16-16-16-16z"></path></svg>
     ),
   },
   {
@@ -223,6 +223,9 @@ var listConsejo = [
   "Miembro consejero suplente: Sr. Cárdenas Reynaga, José Antonio.",
   "Miembro consejero suplente: Sr. Cumpa Reyes, Jorge Segundo.",
   "Miembro consejero suplente: Sra. Hoyos Quiroz, Kattya Karyna.",
+];
+
+var listSecretariasArbitrales = [
   "Secretaria General y Secretaria Arbitral: Abg. María Alejandra Pasco Herrera.",
   "Secretaria Arbitral: Abg. Cynthia Chavesta Rodriguez.",
   "Secretaria Arbitral: Abg. Sofia Alejandra Kcomt Fernandez."
@@ -352,6 +355,32 @@ export default function CentroArbitraje() {
           <ul className="list-none list-inside mb-14 text-gray-800">
             {listConsejo.map((it, i) => (
               <li className="flex mb-2" key={i}>
+                <span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-4 w-4 mt-1 text-blue-500 mr-3"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={3}
+                      d="M5 13l4 4L19 7"
+                    />
+                  </svg>
+                </span>{" "}
+                {it}
+              </li>
+            ))}
+          </ul>
+          <p className="font-bold text-themeBlue text-xl mb-6">
+            SECRETARÍAS ARBITRALES:
+          </p>
+          <ul className="list-none list-inside mb-14 text-gray-800">
+            {listSecretariasArbitrales.map((it, i) => (
+              <li className="flex mb-2" key={`sa_${i}`}>
                 <span>
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
